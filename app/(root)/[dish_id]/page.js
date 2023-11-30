4407594c92a98a14735e0d427a3263dd8a9624ef
Vankida <bassemyoussef400@gmail.com>
2023-11-30 19:26:03 +0700

    Finished the "edit dish form"

--- app/(root)/[dish_id]/page.js
+++ app/(root)/[dish_id]/page.js
@@ -17,6 +17,27 @@ function page({ params }) {
   const [dishPage, setDishPage] = useState(1);
   const [dishCategory, setDishCategory] = useState("Wok");
 
+  // Edit dish variables
+  const [newDishName, setNewDishName] = useState("");
+  const [newDishDescription, setNewDishDescription] = useState("");
+  const [newDishPrice, setNewDishPrice] = useState(0);
+  const [newDishImage, setNewDishImage] = useState("");
+  const [newIsDishVegeterian, setNewIsDishVegeterian] = useState(false);
+  const [newDishRating, setNewDishRating] = useState(0);
+  const [newDishPage, setNewDishPage] = useState(1);
+  const [newDishCategory, setNewDishCategory] = useState("Wok");
+  const [formComplete, setFormComplete] = useState(false);
+
+  useEffect(() => {
+    if (newDishName && newDishDescription && newDishImage) {
+      setFormComplete(true);
+    } else {
+      setFormComplete(false);
+    }
+  }, [newDishName, newDishDescription, newDishImage]);
+
+  const [dishBeingEdited, setDishBeingEdited] = useState(false);
+
   useEffect(() => {
     fetch(`/api/dish/${dish_id}`, {
       method: "GET",
@@ -39,22 +60,184 @@ function page({ params }) {
       .catch((error) => console.error("Error fetching the dish:", error));
   }, []);
 
-  return (
-    <div className={styles.card}>
-      <h2>{dishName}</h2>
-      <img src={dishImage} alt={"food"} className={styles.image} />
-      <h4>Dish category: {dishCategory}</h4>
-      {isDishVegeterian ? <h4>Vegeterian</h4> : <h4>Not vegeterian</h4>}
-      <p>{dishDescription}</p>
-      <h4>rating: {dishRating}</h4>
-      <h4>price: {dishPrice}$ / dish</h4>
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-      {isAdmin && (
-        <button type="submit" className={styles.editButton}>
-          Edit Dish
-        </button>
+    // construct the data object to send in the request body
+    const data = {
+      name: dishName,
+      description: dishDescription,
+      price: dishPrice,
+      image: dishImage,
+      vegeterian: isDishVegeterian,
+      rating: dishRating,
+      category: dishCategory,
+      page: dishPage,
+    };
+    console.log(data);
+
+    // const accessToken = localStorage.getItem("token");
+    // fetch("/api/dish", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Bearer ${accessToken}`,
+    //   },
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data); // success
+    //   })
+    //   .catch((error) => console.error("Error creating dish:", error));
+  };
+
+  return (
+    <>
+      {!dishBeingEdited && (
+        <div className={styles.card}>
+          <h2>{dishName}</h2>
+          <img src={dishImage} alt={"food"} className={styles.image} />
+          <h4>Dish category: {dishCategory}</h4>
+          {isDishVegeterian ? <h4>Vegeterian</h4> : <h4>Not vegeterian</h4>}
+          <p>{dishDescription}</p>
+          <h4>rating: {dishRating}</h4>
+          <h4>price: {dishPrice}$ / dish</h4>
+
+          {isAdmin && (
+            <button
+              type="submit"
+              className={styles.editButton}
+              onClick={() => setDishBeingEdited(true)}
+            >
+              Edit Dish
+            </button>
+          )}
+        </div>
       )}
-    </div>
+      {dishBeingEdited && (
+        <div className={styles.container}>
+          {isAdmin && (
+            <button
+              type="submit"
+              className={styles.editButton}
+              onClick={() => setDishBeingEdited(false)}
+            >
+              Cancel
+            </button>
+          )}
+          <h1 className={styles.h1}>Edit Dish Info</h1>
+          <div className={styles.form}>
+            <div className={styles.inputContainer}>
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                <b>Dish Name</b>
+              </label>
+              <input
+                type="text"
+                className={styles.input}
+                placeholder="Dish Name"
+                value={newDishName}
+                onChange={(e) => setNewDishName(e.target.value)}
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                <b>Dish Description</b>
+              </label>
+              <input
+                type="text"
+                className={styles.input}
+                placeholder="Dish Description"
+                value={newDishDescription}
+                onChange={(e) => setNewDishDescription(e.target.value)}
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                <b>Dish Price</b>
+              </label>
+              <input
+                type="number"
+                className={styles.input}
+                placeholder="Dish Price"
+                value={newDishPrice}
+                onChange={(e) => setNewDishPrice(e.target.value)}
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                <b>Dish Image Link</b>
+              </label>
+              <input
+                type="text"
+                className={styles.input}
+                placeholder="Dish Image"
+                value={newDishImage}
+                onChange={(e) => setNewDishImage(e.target.value)}
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                <b>Vegeterian?</b>
+              </label>
+              <input
+                type="checkbox"
+                value={newIsDishVegeterian}
+                onChange={(e) => setNewIsDishVegeterian(e.target.checked)}
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                <b>Dish Rating</b>
+              </label>
+              <input
+                type="number"
+                className={styles.input}
+                placeholder="Dish Rating"
+                value={newDishRating}
+                onChange={(e) => setNewDishRating(e.target.value)}
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                <b>Dish Page</b>
+              </label>
+              <input
+                type="number"
+                className={styles.input}
+                placeholder="Dish Page"
+                value={newDishPage}
+                onChange={(e) => setNewDishPage(e.target.value)}
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                <b>Dish Category</b>
+              </label>
+              <select
+                className={styles.select}
+                value={newDishCategory}
+                onChange={(e) => setNewDishCategory(e.target.value)}
+              >
+                <option className={styles.option}>Wok</option>
+                <option className={styles.option}>Pizza</option>
+                <option className={styles.option}>Soup</option>
+                <option className={styles.option}>Dessert</option>
+                <option className={styles.option}>Drink</option>
+              </select>
+            </div>
+            <button
+              type="submit"
+              className={styles.registerButton}
+              disabled={!formComplete}
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
