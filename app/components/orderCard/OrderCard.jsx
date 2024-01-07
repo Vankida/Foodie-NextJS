372a6fe514603a189199253b8c8852e080420652
SinisterSan <138826506+SinisterSan@users.noreply.github.com>
2024-01-07 20:11:21 +0700

    finished the confirm order api

--- app/components/orderCard/OrderCard.jsx
+++ app/components/orderCard/OrderCard.jsx
@@ -4,11 +4,30 @@ import styles from "./orderCard.module.css";
 import Link from "next/link";
 
 function OrderCard({ orderID, orderDate, deliveryTime, status }) {
+  const confirmOrder = (e) => {
+    e.preventDefault();
+    const accessToken = localStorage.getItem("token");
+
+    fetch(`/api/order/${orderID}/status`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); // success
+      })
+      .catch((error) => console.error("Error getting order:", error));
+  };
+
   return (
     <Link href={`/order/${orderID}`} className={styles.card}>
       <h2>Order from: {orderDate}</h2>
       <p>Delivery time: {deliveryTime}</p>
       <p>Status: {status}</p>
+      <button onClick={(e) => confirmOrder(e)}>Confirm Order</button>
     </Link>
   );
 }
